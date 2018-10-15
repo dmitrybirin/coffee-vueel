e708f6c21a3387e09e24372baa9c17c193523197
Dmitry Birin <dmitry.birin@gmail.com>
2018-10-16 00:57:58 +0300

    updating styles of points

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -3,27 +3,27 @@
 </template>
 
 <script>
-import Chart from 'chart.js';
+import Chart from "chart.js";
 
-import wheel from '../models/wheel';
+import wheel from "../models/wheel";
 
-const fillColor = '#ffcb0f';
-const borderColor = '#663c1f'
+const fillColor = "#ffcb0f";
+const borderColor = "#663c1f";
 
 const initData = {
   data: {
     labels: Object.keys(wheel),
     datasets: [
       {
-        pointStyle: 'circle',
+        pointStyle: "circle",
         pointRadius: 5,
-        label: 'coffeewheel',
+        label: "coffeewheel",
         backgroundColor: fillColor,
         borderColor: borderColor,
         pointBackgroundColor: fillColor,
         pointBorderColor: borderColor,
         data: Object.values(wheel)
-      },
+      }
     ]
   },
   options: {
@@ -34,14 +34,24 @@ const initData = {
         max: 5,
         min: 0,
         stepSize: 1
-      },
+      }
     },
-    events: ['mousedown', 'mouseup', 'mousemove'],
+    tooltips: {
+        // enabled: false,
+    },    
+    elements: {
+        point: {
+            radius: 2,
+            hoverRadius: 10
+        },
+    },
+    
+    events: ["mousedown", "mouseup", "mousemove"]
   }
 };
 
 export default {
-  name: 'Chart',
+  name: "Chart",
   computed: {
     chartPoints: () => Object.values(wheel)
   },
@@ -56,68 +66,69 @@ export default {
     wheel,
     chart: null,
     drag: {
-        status: false,
-        label: null,
-        idx: 0,
-    },
+      status: false,
+      label: null,
+      idx: 0
+    }
   }),
   methods: {
     createChart(chartId, chartData) {
       const ctx = document.getElementById(chartId);
       this.chart = new Chart(ctx, {
-        type: 'radar',
+        type: "radar",
         data: chartData.data,
         options: chartData.options
       });
     },
-    rangeValue(value){
-        const { max, min } = this.chart.scale
-        if (value > max) return max
-        if (value < min) return min
-        return value
+    rangeValue(value) {
+      const { max, min } = this.chart.scale;
+      if (value > max) return max;
+      if (value < min) return min;
+      return value;
     },
-    getValueFromPoint(x,y){
-        const { scale } = this.chart
-        const angleRad = scale.getIndexAngle(this.drag.idx)
-        const angle = angleRad * 180/ Math.PI
-        let dist;
-        if (angleRad === Math.PI/2 || angleRad === 3*Math.PI/2) {
-          dist = (x-scale.xCenter)/Math.sin(angleRad)
-        } else {
-          dist = (scale.yCenter-y)/Math.cos(angleRad)
-        }
-        
-        const scalingFactor = scale.drawingArea / (scale.max - scale.min)
-        const value = (dist/scalingFactor ) + scale.min
-        return this.rangeValue(value)
-    },
-    handleClick(e, arr){
-    
-    if (e.type === 'mousedown' && arr.length!==0) {
-        this.drag.status = true;
-        const { scale } = this.chart
-        
-        const chartData = arr[0]['_chart'].config.data;
-        this.drag.idx = arr[0]['_index'];
-        this.drag.label = chartData.labels[this.drag.idx];
-    }
-    if (e.type === 'mousemove' && this.drag.status) {
-        wheel.changeItem(this.drag.label, String(this.getValueFromPoint(e.layerX, e.layerY)))
-    }
+    getValueFromPoint(x, y) {
+      const { scale } = this.chart;
+      const angleRad = scale.getIndexAngle(this.drag.idx);
+      const angle = (angleRad * 180) / Math.PI;
+      let dist;
+      if (angleRad === Math.PI / 2 || angleRad === (3 * Math.PI) / 2) {
+        dist = (x - scale.xCenter) / Math.sin(angleRad);
+      } else {
+        dist = (scale.yCenter - y) / Math.cos(angleRad);
+      }
 
-    if (e.type === 'mouseup' && this.drag.status) {
-        this.drag.status = false;
-        
-        const roundedValue = Math.round(this.wheel[this.drag.label])
-        this.wheel.changeItem(this.drag.label, String(roundedValue))
-        
-    }
+      const scalingFactor = scale.drawingArea / (scale.max - scale.min);
+      const value = dist / scalingFactor + scale.min;
+      return this.rangeValue(value);
     },
+    handleClick(e, arr) {
+      if (e.type === "mousedown" && arr.length !== 0) {
+        this.drag.status = true;
+        const { scale } = this.chart;
+
+        const chartData = arr[0]["_chart"].config.data;
+        this.drag.idx = arr[0]["_index"];
+        this.drag.label = chartData.labels[this.drag.idx];
+      }
+      if (e.type === "mousemove" && this.drag.status) {
+        wheel.changeItem(
+          this.drag.label,
+          String(this.getValueFromPoint(e.layerX, e.layerY))
+        );
+      }
+
+      if (e.type === "mouseup" && this.drag.status) {
+        this.drag.status = false;
+
+        const roundedValue = Math.round(this.wheel[this.drag.label]);
+        this.wheel.changeItem(this.drag.label, String(roundedValue));
+      }
+    }
   },
   mounted() {
-    initData.options.onHover = this.handleClick
+    initData.options.onHover = this.handleClick;
 
-    this.createChart('coffee-wheel', initData);
+    this.createChart("coffee-wheel", initData);
   }
 };
 </script>
