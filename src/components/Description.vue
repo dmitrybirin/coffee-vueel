7ac45e65b5d1bd993887cdfbb720ab9ad0a28484
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-20 13:13:18 +0300

    title styling description deleted

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,17 +1,15 @@
 <template>
     <div id='container'>
-		<input 
-			type="text" 
-			@value="cup.title"
-			@input="e => cup.changeTitle(e.target.value)"
-		/>
-
-		<textarea 
-			type="text" 
-			rows="3" cols="33"
-			@value="cup.description"
-			@input="e => cup.changeDescription(e.target.value)"
-		/>
+		<div id="title-container">
+			<input 
+				id="title"
+				placeholder="name your coffee"
+				type="text" 
+				@value="cup.title"
+				@input="e => cup.changeTitle(e.target.value)"
+			/>
+			<label for="title">Name your coffee</label>
+		</div>
     </div>
 </template>
 
@@ -27,8 +25,42 @@ export default {
 </script>
 
 <style scoped>
-div#container {
+div#container div#title-container {
     display: flex;
     flex-direction: column;
+	margin: 25px;
 }
+
+div#title-container {
+	position: relative;
+    padding-top: 2rem;
+}
+
+div#title-container label {
+    position: absolute;
+    top: 0;
+    font-size: var(--font-size-small);
+    opacity: 1;
+    transform: translateY(0);
+    transition: all 0.2s ease-out;
+	font-size: 20px;
+}
+
+input#title:placeholder-shown + label {
+    opacity: 0;
+    transform: translateY(1rem);
+}
+
+input#title::placeholder {
+	font-size: 30px;
+	padding: 5px;
+	padding-top: 10px;
+}
+
+input#title {
+	border: 0;
+	height: 50px;
+	font-size: 30px;
+}
+
 </style>
