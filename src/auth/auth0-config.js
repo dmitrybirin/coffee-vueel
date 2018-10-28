7c3a3e527137baf16ae95233c2d32cb40db266e6
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-28 20:19:46 +0200

    auth0 config fixed and refactored

--- src/auth/auth0-config.js
+++ src/auth/auth0-config.js
@@ -1,10 +1,10 @@
+const devUrl = 'http://localhost:8080'
+const prodUrl =  'https://coffeewheel.xyz'
+
 export const AUTH_CONFIG = {
 	clientId: 'sjCx8tDOXrkydooBnvyMCClbQO8t1MwS',
 	domain: 'cofferver.eu.auth0.com',
-	callbackUrl:
-		process.env.NODE_ENV === 'dev'
-			? 'https://localhost:8080/callback'
-			: 'https://coffeewheel.xyz/callback',
+	callbackUrl: `${process.env.NODE_ENV === 'dev' ? devUrl : prodUrl}/callback`,
 	apiUrl: 'https://api.coffeewheel.xyz',
-	returnUrl:'http://localhost:8080',
+	returnUrl: `${process.env.NODE_ENV === 'dev' ? devUrl : prodUrl}`,
 }
