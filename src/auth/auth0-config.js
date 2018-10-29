584f1d7ce194c0aab7da93fa8c8bbfea4cd1d25c
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-29 22:54:50 +0200

    ... and config

--- src/auth/auth0-config.js
+++ src/auth/auth0-config.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
 	clientId: 'sjCx8tDOXrkydooBnvyMCClbQO8t1MwS',
 	domain: 'cofferver.eu.auth0.com',
-	callbackUrl: `${process.env.SITE_URL}/callback`,
+	callbackUrl: `${process.env.VUE_APP_SITE_URL}/callback`,
 	apiUrl: 'https://api.coffeewheel.xyz',
-	returnUrl: process.env.SITE_URL,
+	returnUrl: process.env.VUE_APP_SITE_URL,
 }
