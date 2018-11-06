b822a898f227423be7c37276524f732c995c655e
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-11-06 23:39:17 +0200

    create some meta info for the router

--- src/router.js
+++ src/router.js
@@ -7,13 +7,51 @@ import Callback from './components/Callback'
 
 Vue.use(VueRouter)
 
+const getMetaTags = description => ({
+	metaTags: [
+		{
+			name: 'description',
+			content: description,
+		},
+		{
+			property: 'og:description',
+			content: description,
+		},
+	],
+})
+
 const router = new VueRouter({
 	mode: 'history',
 	history: true,
 	routes: [
-		{ path: '/list', component: List },
-		{ path: '/new', component: New },
-		{ path: '/callback', component: Callback },
+		{
+			path: '/list',
+			component: List,
+		},
+		{
+			path: '/new',
+			component: New,
+			meta: {
+				title: 'New Cup',
+				metaTags: getMetaTags('Add a new cup to the list'),
+			},
+		},
+		{
+			path: '/edit',
+			component: New,
+			meta: {
+				title: 'Edit Cup',
+				metaTags: getMetaTags('Edit the cup'),
+			},
+		},
+		{
+			path: '/callback',
+			component: Callback,
+			meta: {
+				title: 'Brewing your login',
+				metaTags: getMetaTags('Authentification progress'),
+			},
+		},
 	],
 })
 
